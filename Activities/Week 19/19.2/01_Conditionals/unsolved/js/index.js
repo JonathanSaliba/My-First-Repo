f73fa480ffea987c0dcb247d38936049b07eeeca
JonathanSaliba <Jonathan.saliba@ontariotechu.net>
2022-05-11 20:40:55 -0400

    19.2 activities completed

--- Activities/Week 19/19.2/01_Conditionals/unsolved/js/index.js
+++ Activities/Week 19/19.2/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,19 @@
+
 $('.tab').on("click", function(){
-  
+    var isTab1=$(this).hasClass("tab1");
+    var isTab2=$(this).hasClass("tab2");
+
+    if(isTab1){
+        $(".tab1").addClass("active");
+        $(".tab2").removeClass("active");
+        $("#tabContent1").css("display", "block");
+        $("#tabContent2").css("display", "none");
+    }
+
+    else if(isTab2) {
+        $(".tab2").addClass("active");
+        $(".tab1").removeClass("active");
+        $("#tabContent2").css("display", "block");
+        $("#tabContent1").css("display", "none");
+    }
 });
